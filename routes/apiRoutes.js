33fd0e42a745e91f5683d05c51443b83adf9fecd
jocelynrem <jocelynrem@gmail.com>
2021-06-09 15:27:44 -0400

    some progress in notes.html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,4 +2,10 @@ const notesData = require("../db/db.json");
 
 module.exports = app => {
     app.get('/api/notes', (req, res) => res.json(notesData));
+
+    app.post('/api/notes', (req, res) => {
+          notesData.push(req.body);
+          res.json('note push worked');
+  
+      });
 }
--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./routes/apiRoutes")(app)
-require("./routes/htmlRoutes")(app)
+require("./routes/htmlRoutes.js")(app)
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
